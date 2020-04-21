65b9f0344a9ae27d2f4db97be565cf112dcee8fd
Lucas Rosa <lucasrsilva1604@gmail.com>
2020-04-20 21:16:09 -0300

    added image preview to page, fixing exif.getdata issues
    Signed-off-by: Lucas Rosa <lucasrsilva1604@gmail.com>

--- js/fetch-metadata.js
+++ js/fetch-metadata.js
@@ -1,17 +1,36 @@
 $(document).ready(function() {
-  btn = document.getElementById("submit-btn");
-  btn.addEventListener("click", function(){
-    alert("cccc");
-    var img = document.getElementById("uploaded-img");
-    EXIF.getData(img, function() {
-      alert("gotten");
-      alert("gotten");
-      alert("gotten");
-      alert("gotten");
-      alert("gotten");
-      alert("gotten");
-    });
-  }, false);
+
+  // Prevent form to reload page on submit
+  $(".form-inline").submit(function(e) {
+    e.preventDefault();
+  });
+
+  // Populate and show image preview
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function(e) {
+        $('#preview-img').attr('src', e.target.result);
+        $('#preview-img').show()
+      }
+
+      reader.readAsDataURL(input.files[0]); // convert to base64 string
+    }
+  }
+
+  // Add preview to onChange
+  $("#uploaded-img").change(function() {
+    readURL(this);
+  });
+
+  // Add metadata fetcher to submit button
+  document.getElementById("submit-btn")
+    .addEventListener("click", function(){
+      EXIF.getData(document.getElementById("preview-img"), function() {
+        alert("gotten");
+      });
+    }, false);
 
   // $(document).on("click", "#submit-btn", function() {
   //   console.log("CLICKED");
